9c90edf9073bd01449248fd0512ca617ebeccf35
Jeremy <jeremyash13@gmail.com>
2018-09-03 18:45:49 -0600

    Fixed to follow project guidelines
    i had coded the caption highlighting feature to get the cue timings from the VTT file itself, not realizing a server was necessary to load VTT files.
    
    so i changed it to get the cue timings from the elements data attribute instead.

--- js/main.js
+++ js/main.js
@@ -1,7 +1,9 @@
 //Variables
-let mainVideo = document.querySelector('.main__video--main');
-let capTrack = document.querySelector('.main__video--main').textTracks[0];
-let spanCollection = document.querySelectorAll('span');
+const mainVideo = document.querySelector('.main__video--main');
+const capTrack = document.querySelector('.main__video--main').textTracks[0];
+const spanCollection = document.querySelectorAll('span');
+const textContainer = document.querySelector('.main__wrapper--text');
+
 
 //Hook all audio & video elements, call MediaElementJS
 $('video, audio').mediaelementplayer({
@@ -9,24 +11,46 @@ $('video, audio').mediaelementplayer({
   stretching: 'responsive'
 });
 
+//Highlights text as video plays, by hardcoded cue times
+//within element's data-start and data-end attributes
+mainVideo.addEventListener('timeupdate', (e) => {
+  let time = e.target.currentTime;
 
-const highlightText = () => {
-  //Searchs the cue list for a match with active cue,
-  //if match is found, cues[i] corresponds perfectly with spanCollection[i],
-  //letting me know exactly which span element within the collection needs the
-  //'.caption-highlight' class
-
-  let i = 0;
-  for (i=0; i<capTrack.cues.length; i+=1) {
-    if (capTrack.activeCues[0].text === capTrack.cues[i].text) {
+  for (let i = 0; i < spanCollection.length; i += 1) {
+    if (time >= spanCollection[i].dataset.start && time <= spanCollection[i].dataset.end) {
       spanCollection[i].className = 'caption-highlight';
     } else {
       spanCollection[i].className = '';
     }
   }
-  mainVideo.addEventListener('ended', () => {
-    spanCollection[i-1].className = '';
-  });
-};
+});
 
-mainVideo.addEventListener('timeupdate', highlightText);
+//Seeks to time in video by clicking on text
+textContainer.addEventListener('click', (e) => {
+  if (e.target.tagName === 'SPAN') {
+    mainVideo.currentTime = e.target.dataset.start;
+  }
+});
+
+
+// Highlights text as video plays, by getting the cue times from the VTT file
+// requires a server to work, therefore violating project guidelines
+// const highlightText = () => {
+//   //Searchs cues[] (the cue list), for a match with active cue.
+//   //if match is found, cues[i] corresponds perfectly with spanCollection[i].
+//   //letting me know exactly which span element within the collection needs the
+//   //'.caption-highlight' class.
+//
+//   let i = 0;
+//   for (i=0; i<capTrack.cues.length; i+=1) {
+//     if (capTrack.activeCues[0].text === capTrack.cues[i].text) {
+//       spanCollection[i].className = 'caption-highlight';
+//     } else {
+//       spanCollection[i].className = '';
+//     }
+//   }
+//   mainVideo.addEventListener('ended', () => {
+//     spanCollection[i-1].className = '';
+//   });
+// };
+// mainVideo.addEventListener('timeupdate', highlightText);
